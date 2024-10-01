b40dabf5870970d69f00642dbba08ba935e864ec
devyan18 <epache17@gmail.com>
2024-10-01 13:53:01 -0300

    clear main.py

--- main.py
+++ main.py
@@ -1,30 +1,5 @@
-from langchain_chroma import Chroma
-from src.retriever import chroma_client, INDEX_NAME
-from langchain_community.embeddings import OllamaEmbeddings
 from src.config import env
 from src.chain import chain
-from langchain.chains.question_answering import load_qa_chain
-from src.llm import llm
-
-# vstore = Chroma(client=chroma_client,
-#                 collection_name=INDEX_NAME,
-#                 embedding_function=OllamaEmbeddings(
-#                 model=env.model,
-#             ))
-
-
-# docs = vstore.similarity_search(question, 5)
-
-# chain = load_qa_chain(llm, chain_type="stuff")
-
-# respuesta = chain.invoke(
-#     input=question,
-#     context=docs,
-# )
-
-
-
-# print(respuesta)
 
 def main():
     try:
@@ -39,8 +14,3 @@ def main():
         print(f"Goodbye! {e}")
 
 main()
-
-# def delete_index():
-#     chroma_client.delete_collection(INDEX_NAME)
-
-# delete_index()
